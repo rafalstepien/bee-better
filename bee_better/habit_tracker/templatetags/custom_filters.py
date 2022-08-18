8fe0c7ca0f7ad9357f07d494cac84bc2c8d9deca
Rafał Stępień <rafal.stepien@stxnext.pl>
2022-08-17 22:09:15 -0400

    Add JS code responsible for changing the color of the column

--- bee_better/habit_tracker/templatetags/custom_filters.py
+++ bee_better/habit_tracker/templatetags/custom_filters.py
@@ -7,9 +7,12 @@ register = template.Library()
 
 @register.filter()
 def cell_conditional_formatting(variable: Union[bool, None]):
+    """
+    Update background color of the cell depends on value.
+    """
     if variable:
-        return "table-success"  # True
+        return "background-color: #bde9ba;"  # if cell has value of "True"
     else:
         if variable == "":
-            return "table-warning"  # None
-        return "table-danger"  # False
+            return "background-color: #00A9CB;"  # if cell has no value
+        return "background-color: #f57c67;"  # if cell has value of "False"
