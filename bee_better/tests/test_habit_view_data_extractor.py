52bdcf84b57c3f8b3fec64aa47f62bb78017f772
Rafał Stępień <rafal.stepien@stxnext.pl>
2022-08-28 09:29:04 -0400

    Add missing tests for data extractor

--- bee_better/tests/test_habit_view_data_extractor.py
+++ bee_better/tests/test_habit_view_data_extractor.py
@@ -1,5 +1,6 @@
 from freezegun import freeze_time
 from habit_tracker.views.habits_view_data_extractor import HabitsViewDataExtractor
+from habit_tracker.models import ParsedAJAXRequestData
 
 
 @freeze_time("2022-08-28")
@@ -22,13 +23,34 @@ def test_extractor_initializes_correctly(mocker, example_list_of_habits):
     assert len(extractor.habits) == 3
 
 
-def test_get_rows_for_table():
-    pass
+def test_get_rows_for_table(mocker, example_list_of_habits):
+    mocker.patch("habit_tracker.models.Habit.objects.filter", return_value=example_list_of_habits)
+    mocker.patch("habit_tracker.models.HabitTrack.objects.filter", return_value=[])
+
+    extractor = HabitsViewDataExtractor(1)
+
+    assert extractor.get_rows_for_table() == [
+        ["habit_1", "", "", "", "", "", "", "", ""],
+        ["habit_2", "", "", "", "", "", "", "", ""],
+        ["habit_3", "", "", "", "", "", "", "", ""],
+    ]
 
 
-def test_get_all_habits_name():
-    pass
+def test_get_all_habits_names(mocker, example_list_of_habits):
+    mocker.patch("habit_tracker.models.Habit.objects.filter", return_value=example_list_of_habits)
+    assert HabitsViewDataExtractor(1).get_all_habit_names_for_user() == ['habit_1', 'habit_2', 'habit_3']
 
 
-def test_get_habit_data_from_request_body():
-    pass
+def test_get_habit_data_from_request_body(mocker, example_list_of_habits):
+    mocker.patch("habit_tracker.models.Habit.objects.filter", return_value=example_list_of_habits)
+
+    data = HabitsViewDataExtractor(1).get_habit_data_from_request_body(
+        bytes("element_id=2-3&habitDone=False", "utf-8")
+    )
+
+    assert data == ParsedAJAXRequestData(
+        name="habit_2",
+        date="2022-08-23",
+        value=False
+    )
+
