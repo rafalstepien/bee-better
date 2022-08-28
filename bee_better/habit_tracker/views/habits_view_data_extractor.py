2e016d4a97711ff52b0f7be4b4f68189d09dd9f9
Rafał Stępień <rafal.stepien@stxnext.pl>
2022-08-27 22:25:14 -0400

    Make action of clicking the cell return habit name and date

--- bee_better/habit_tracker/views/habits_view_data_extractor.py
+++ bee_better/habit_tracker/views/habits_view_data_extractor.py
@@ -1,5 +1,5 @@
 import datetime
-from typing import List
+from typing import List, Tuple
 
 from habit_tracker.models import Habit, HabitTrack
 
@@ -16,8 +16,28 @@ class HabitsViewDataExtractor:
     def __init__(self, user_id: int):
         self.user_id = user_id
         self.columns = None
+        self.habits = Habit.objects.filter(user__id=self.user_id)
 
         self._get_habit_column_names()
+
+    def get_rows_for_table(self) -> List:
+        """
+        Get the rows for main habit table.
+        """
+        return [self._get_one_row_for_table(habit) for habit in self.habits]
+
+    def get_all_habit_names_for_user(self) -> List[str]:
+        return [habit.habit_name for habit in self.habits]
+
+    def get_habit_name_and_date_from_request_body(self, request_body: bytes) -> Tuple[str, str]:
+        row_id, column_id = self._extract_row_and_column_id(request_body)
+        column_names = self.columns
+        row_names = self.get_all_habit_names_for_user()
+        return row_names[row_id], column_names[column_id]
+
+    @staticmethod
+    def _extract_row_and_column_id(request_body: bytes) -> List[int]:
+        return [int(id_number) - 1 for id_number in request_body.decode("utf-8").split("-")]
 
     def _get_habit_column_names(self):
         """
@@ -33,13 +53,6 @@ class HabitsViewDataExtractor:
             for num_days in range(1, self.NUMBER_OF_DAYS_TO_DISPLAY_AFTER)
         ]
         self.columns = ["Habit name"] + dates_before_today + dates_after_today
-
-    def get_rows_for_table(self) -> List:
-        """
-        Get the rows for main habit table.
-        """
-        habits_for_given_user = Habit.objects.filter(user__id=self.user_id)
-        return [self._get_one_row_for_table(habit) for habit in habits_for_given_user]
 
     def _get_one_row_for_table(self, habit: Habit):
         """
--- bee_better/habit_tracker/views/views.py
+++ bee_better/habit_tracker/views/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from habit_tracker import utils
+from habit_tracker.utils import is_ajax
 from habit_tracker.views.habits_view_data_extractor import HabitsViewDataExtractor
 
 
@@ -8,9 +8,7 @@ def index(request):
 
 
 def habits(request):
-    if request.method == "POST":
-        pass
-    else:
+    if request.method == "GET":
         data_extractor = HabitsViewDataExtractor(request.user.id)
         context = {
             "column_names": data_extractor.columns,
@@ -20,5 +18,8 @@ def habits(request):
 
 
 def update_habit(request):
-    print(request)
-    pass
+    if is_ajax(request):
+        habit_name, habit_date = HabitsViewDataExtractor(request.user.id).get_habit_name_and_date_from_request_body(
+            request.body
+        )
+        print(habit_name, habit_date)
